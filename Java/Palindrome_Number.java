8bef3cc6d8c25ca5520639c4f6dd555c97cc2084
Abhinav Agrawal <104521563+Engg-Abhinav@users.noreply.github.com>
2023-06-15 23:58:17 +0530

    Palindrome Number

--- Java/Palindrome_Number.java
+++ Java/Palindrome_Number.java
@@ -7,7 +7,7 @@ class Solution {
         int reverse = 0;
         while(number>0){
             reverse = reverse*10 + number%10;
-            number/=10
+            number/=10;
         }
         return x == reverse;
     }
